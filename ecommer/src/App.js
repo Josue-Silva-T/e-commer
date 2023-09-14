3637edfb46b33b2b2d7723a253f0019fdcd016aa
Josue-Silva-T <sgjosue4@gmail.com>
2023-09-13 19:34:29 -0600

    Proyecto migrado a react

---
+++ ecommer/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FaSistrix, FaCartShopping, FaLocationDot } from "react-icons/fa6";
+
+class App extends Component {
+  render() {
+    return (
+      <div className='header'>
+      <a className="logotipo" href="index.html">PRIME</a>
+      <a className="ubicacion" href="#"><FaLocationDot/>Elige tu dirección</a>
+      <div class="buscador">
+          <a href="#" className="categoria">Todas las categorias</a>
+          <input className='lector' type='text' placeholder='Buscar en prime'></input>
+          <a href="#" className="boton_buscar">< FaSistrix /></a>
+          
+      </div>
+      <a className="carrito" href="#"><FaCartShopping/></a>
+  </div>
+    );
+  }
+}
+
+export default App;
