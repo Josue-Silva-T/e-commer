439541af995b46042fad6d40f56cdbf7f76179e4
Josue-Silva-T <sgjosue4@gmail.com>
2023-09-18 16:17:11 -0600

    improvement from Home

--- ecommer/src/Home.js
+++ ecommer/src/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import background from './img/prime.png';
 import './Home.css';
+import lentes from './img/lentes.png';
 
 export function Home(){
     return <div className="home">
+        <h1>PRIME</h1>
+        <img src={lentes} alt='Glasses'></img>
     </div>
 }
