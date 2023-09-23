d795c19986618a258ee42f4a037d23af572ab88f
Urielxdov <122407570+Urielxdov@users.noreplu.github.com>
2023-09-22 23:16:07 -0600

    Added router and modified the arquitecture

---
+++ ecommer/src/Componentes/Struct_component/Struct.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaSistrix, FaCartShopping, FaLocationDot } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import './Struct.component.css';
+
+export function Struct() {
+  return (
+    <div className='header'>
+      <Link className="logotipo" to='/'>PRIME</Link>
+      <Link className="ubicacion" to='#'><FaLocationDot />Elige tu dirección</Link>
+      <div className="buscador">
+        <select className='categoria' id='categoria' defaultValue="0">
+          <option value="0">Todas las categorías</option>
+          <option value="1">Probando</option>
+        </select>
+        <input className='lector' type='text' placeholder='Buscar en prime' />
+        <Link to='#' className="boton_buscar">< FaSistrix /></Link>
+      </div>
+      <Link className="carrito" to='/carrito'><FaCartShopping /></Link>
+    </div>
+  )
+}
