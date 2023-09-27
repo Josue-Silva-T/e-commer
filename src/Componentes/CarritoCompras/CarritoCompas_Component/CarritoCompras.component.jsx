64cc65346a562f27d402eb9941f0cf94048f69ec
Urielxdov <122407570+Urielxdov@users.noreply.github.com>
2023-09-27 11:42:58 -0600

    Se refactorizo el carrito

--- src/Componentes/CarritoCompras/CarritoCompas_Component/CarritoCompras.component.jsx
+++ src/Componentes/CarritoCompras/CarritoCompas_Component/CarritoCompras.component.jsx
@@ -25,49 +25,67 @@ export function CarritoCompras() {
         setFavorito(favoritos);
     }, []);
 
+    {/**Se suman los costos para obtener un total */ }
     const totalPagar = comprasEnCarrito.reduce((total, compra) => {
         return total + compra.precio * compra.cantidad;
     }, 0);
 
-    if (comprasEnCarrito.length === 0) {
-        return <h2>No hay compras en el carrito aún</h2>;
-    }
+    {/**Si no hay carrito mostramos un mensaje de que esta vacio*/ }
+    const mostrarCarrito = comprasEnCarrito.length === 0 ?
+        <h2>No hay compras en el carrito aún</h2> :
+        comprasEnCarrito.map((compra, index) => (
+            <div key={"carrito" + index}>
+                <VerificarCarrito
+                    id={compra.id}
+                    titulo={compra.titulo}
+                    descripcion={compra.descripcion}
+                    precio={compra.precio}
+                    cantidad={compra.cantidad}
+                    comprasEnCarrito={comprasEnCarrito}
+                    setComprasEnCarrito={setComprasEnCarrito}
+                />
+            </div>
+        ));
+
+    {/**En caso de haber productos en el carrito comenzamos a sumar */}
+    const mostrandoTicket = comprasEnCarrito!==0 ?
+        comprasEnCarrito.map((compra, index) => (
+            <li key={"compra" + index} className='pago-item'>
+                <TotalPagar
+                    titulo={compra.titulo}
+                    precio={compra.precio}
+                    cantidad={compra.cantidad}
+                />
+            </li>
+        )) :
+        <h2>No hay compras</h2>;
+
+    {/**En caso de haber favoritos le damos valores a mostrar en el DOM */}
+    const mostrarFavoritos = favorito ?
+        favorito.map((fav, index) => (
+            <MostrarProductos
+                key={"favorito" + index}
+                nombre={fav.nombre}
+                precio={fav.precio}
+                estado={fav.estado}
+            />
+        )):
+        <h2>No hay favoritos por mostrar</h2>;
+
 
     return (
         <div>
             <section className="contenedor">
                 <div className="contenedor-carrito">
-                    {comprasEnCarrito.map((compra, index) => (
-                        <div key={"carrito" + index}>
-                            <VerificarCarrito
-                                id={compra.id}
-                                titulo={compra.titulo}
-                                descripcion={compra.descripcion}
-                                precio={compra.precio}
-                                cantidad={compra.cantidad}
-                                comprasEnCarrito={comprasEnCarrito}
-                                setComprasEnCarrito={setComprasEnCarrito}
-                            />
-                        </div>
-                    ))}
+                    {mostrarCarrito}
                 </div>
 
                 <article className='contenedor_pago'>
                     <h2>Total a pagar: {totalPagar}</h2>
                     <ul>
-                        {comprasEnCarrito.map((compra, index) => (
-                            <li key={"compra" + index} className='pago-item'>
-                                <TotalPagar
-                                    titulo={compra.titulo}
-                                    precio={compra.precio}
-                                    cantidad={compra.cantidad}
-                                />
-                            </li>
-                        ))}
+                        {mostrandoTicket}
                     </ul>
                     <button className="boton boton-comprar" disabled={totalPagar === 0}>Comprar</button>
-
-
 
                 </article>
             </section>
@@ -78,14 +96,7 @@ export function CarritoCompras() {
                 </div>
 
                 <div className='favoritos_items'>
-                    {favorito.map((fav, index) => (
-                        <MostrarProductos
-                            key={"favorito" + index}
-                            nombre={fav.nombre}
-                            precio={fav.precio}
-                            estado={fav.estado}
-                        />
-                    ))}
+                    {mostrarFavoritos}
                 </div>
             </section>
 
