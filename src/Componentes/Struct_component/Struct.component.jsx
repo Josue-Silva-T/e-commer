1180e58347cf6fbcee8c795d012b43d8b701bac6
Josue-Silva-T <sgjosue4@gmail.com>
2023-09-27 21:10:03 -0600

    New style from page

--- src/Componentes/Struct_component/Struct.component.jsx
+++ src/Componentes/Struct_component/Struct.component.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import { FaSistrix, FaCartShopping, FaLocationDot } from "react-icons/fa6";
+import { FaSistrix, FaCartShopping, FaUser } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import './Struct.component.css';
 import '../../Utileria/normalize.css';
+import '../../Utileria/teme.css'
 
 export function Struct() {
   return (
     <div className='header'>
       <Link className="logotipo" to='/'>PRIME</Link>
-      <Link className="ubicacion" to='#'><FaLocationDot />Elige tu dirección</Link>
       <div className="buscador">
-        <select className='categoria' id='categoria' defaultValue="0">
-          <option value="0">Todas las categorías</option>
-          <option value="1">Probando</option>
-        </select>
         <input className='lector' type='text' placeholder='Buscar en prime' />
         <Link to='#' className="boton_buscar">< FaSistrix /></Link>
       </div>
-      <Link className="carrito" to='/carrito'><FaCartShopping /></Link>
+      <div className="buttons">
+        <Link className="User" to='/'><FaUser /></Link>
+        <Link className="carrito" to='/carrito'><FaCartShopping /></Link>
+      </div>
     </div>
   )
 }
